17785ef897908f444ab344c0d96f18f8d7356326
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-10 11:38:38 -0500

    Did more backend work

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -10,3 +10,8 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NoteSerializerfiels = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
