93394b776c21b146187e59dd9f7b4baf07050bf3
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-14 11:51:55 -0500

    Started on logic in Home.jsx page, change login/register method to noncapitalized

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,26 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+
 function Home() {
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
   return <div>Home</div>;
 }
 
